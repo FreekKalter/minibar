0d22063eb28e07055365bdb526e5d3c36d14d96a
Freek Kalter <freek@kalteronline.org>
2014-07-10 22:33:51 +0200

    automatic deployment with containers

--- fabfile.py
+++ fabfile.py
@@ -26,3 +26,22 @@ def deploy():
 
     # restart service
     sudo('service minibar restart')
+
+def container_deploy():
+    with lcd('webapp'):
+        local('go build')
+
+    local('docker build -t freekkalter/wkiw .')
+    local('docker push freekkalter/wkiw')
+
+    run('docker pull freekkalter/wkiw')
+
+    # run nginx-proy, dont panic if it is already running
+    with settings(hide('warnings'), warn_only=True):
+        run('docker run -d -t -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock \
+                --name=nginx_proxy jwilder/nginx-proxy')
+        run('docker kill wkik_nl')
+
+    run('docker run -d -e VIRTUAL_HOST=wanneerkanikwinnen.nl -v /home/fkalter/minibar-deploy:/logdir\
+            --name=wkik_nl freekkalter/wkiw')
+
